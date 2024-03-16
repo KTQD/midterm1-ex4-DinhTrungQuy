5dc0aed42e51ae8161409acd68cfb9a2d23549a7
DinhTrungQuy <quythks2023@gmail.com>
2024-03-16 14:22:27 +0700

    feat: add feature

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,30 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         int[] numbers = {1, 3, 5, 6, 2, 7, 8, 0, 4, 3, 9, 2, 8, 1, 0, 5, 7, 4, 6, 9, 3, 2, 1, 8, 4, 0, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0};
-    
+
+        Thread sumThread = new Thread(() -> {
+            int sum = 0;
+            for (int number : numbers) {
+                sum += number;
+            }
+            System.out.println("Tổng của các phần tử trong mảng là: " + sum);
+        });
+
+        Thread maxThread = new Thread(() -> {
+//            int max = numbers[0];
+//            for (int number : numbers) {
+//                if (number > max) {
+//                    max = number;
+//                }
+//            }
+//            System.out.println("Phần tử lớn nhất trong mảng là: " + max);
+            System.out.println("Phần tử lớn nhất trong mảng là: " + Arrays.stream(numbers).max().getAsInt());
+        });
+        maxThread.start();
+        sumThread.start();
 
 
-        Viết mã của bạn vào đây
-    
     }
 }
